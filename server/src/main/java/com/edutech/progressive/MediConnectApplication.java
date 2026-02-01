3f78689ff0c4370a8109b11a572048fc96ed1b11
aman223sen <dummy@gmail.com>
2026-02-01 12:19:00 +0000

    Day 3 Task

--- server/src/main/java/com/edutech/progressive/MediConnectApplication.java
+++ server/src/main/java/com/edutech/progressive/MediConnectApplication.java
@@ -1,8 +1,5 @@
 package com.edutech.progressive;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class MediConnectApplication {
     public static void main(String[] args) {
         System.out.println("Hello MediConnect Progressive Project!");
